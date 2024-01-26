8fe0b15bca606c68855515bbe06a0bc59e3ac9af
julyperfect07 <abdallah.july05@gmail.com>
2024-01-26 20:12:14 +0300

    border change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const FooterCom = () => {
   return (
-    <Footer container className=' border-t-8 border border-teal-500'>
+    <Footer container className=' border border-t-8 border-teal-500'>
       <div className=" w-full max-w-7xl mx-auto">
         <div className=" grid w-full justify-between sm:flex md:grid-cols-1">
           <div className=" mt-5">
