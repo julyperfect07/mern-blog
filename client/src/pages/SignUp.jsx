a785b8d9f976b1c72cec507da6ca6ba93de8cd1e
julyperfect07 <abdallah.july05@gmail.com>
2024-01-26 15:16:25 +0300

    create sign in page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
           </form>
           <div className=" flex gap-2 text-sm mt-5">
             <span>Have an account ?</span>
-            <Link to='/sign-up' className=' text-blue-500' >Sign In</Link>
+            <Link to='/sign-in' className=' text-blue-500' >Sign In</Link>
           </div>
         </div>
       </dir>
