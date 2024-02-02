87de8b6903132fecb2c012aa3f89d95f5ac912a6
julyperfect07 <abdallah.july05@gmail.com>
2024-02-02 22:57:45 +0300

    complete profile page ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import DashSidebar from '../components/DashSidebar'
-import DashProfile from '../components/DashProfile'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 const Dashboard = () => {
-  const location = useLocation()
-  const [tab, setTab] = useState('')
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get('tab')
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
     if (tabFromUrl) {
-      setTab(tabFromUrl)
+      setTab(tabFromUrl);
     }
-  }, [location.search])
+  }, [location.search]);
+
   return (
-    <div className=' min-h-screen flex flex-col md:flex-row'>
-      <div className=' md:w-56'>
-        {/* sidebar */}
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
         <DashSidebar />
       </div>
-      <div>
-        {/* profile */}
+      <div className='flex-1'>
         {tab === 'profile' && <DashProfile />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
