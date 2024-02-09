df3ffc4061bdd75e76d3087a9d1c2325612cfbba
julyperfect07 <abdallah.july05@gmail.com>
2024-02-09 20:55:20 +0300

    Complete create post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -5,11 +5,11 @@ export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to create a post'))
   }
-  if (req.body.title || !req.body.content) {
+  if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fields'))
   }
 
-  const slug = req.body.title.split("").join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-')
+  const slug = req.body.title.split(" ").join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '-')
   const newPost = new Post({
     ...req.body,
     slug,
