a1d4facb9420cefd307c62f72063041a1e667d08
julyperfect07 <abdallah.july05@gmail.com>
2024-02-19 16:06:55 +0300

    add delete functionality to the comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 import { Button, Textarea } from 'flowbite-react'
 
-const Comment = ({ comment, onLike, onEdit }) => {
+const Comment = ({ comment, onLike, onEdit, onDelete }) => {
   const { currentUser } = useSelector((state) => state.user)
   const [isEditing, setIsEditing] = useState(false)
   const [editedContent, setEditedContent] = useState(comment.content)
@@ -78,9 +78,14 @@ const Comment = ({ comment, onLike, onEdit }) => {
               </p>
               {
                 currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button onClick={handleEdit} type='button' className=' text-gray-400 hover:text-blue-500'>
-                    Edit
-                  </button>
+                  <>
+                    <button onClick={handleEdit} type='button' className=' text-gray-400 hover:text-blue-500'>
+                      Edit
+                    </button>
+                    <button onClick={() => onDelete(comment._id)} type='button' className=' text-gray-400 hover:text-red-500'>
+                      Delete
+                    </button>
+                  </>
                 )
               }
             </div>
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -48,7 +48,7 @@ const PostPage = () => {
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className='italic'>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
       </div>
-      <div className=' p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{ __html: post && post.content }}>
+      <div className=' p-3 max-w-2xl mx-auto w-full post-content overflow-hidden' dangerouslySetInnerHTML={{ __html: post && post.content }}>
 
       </div>
       <div className=' max-w-4xl mx-auto w-full'>
